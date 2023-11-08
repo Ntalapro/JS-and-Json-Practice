0ef646013ffc449239ebbaabcf7145826f14cb9d
Ntalapro <ncosanetshepiso@gmail.com>
2023-11-08 16:16:37 +0200

    json db added

--- app.js
+++ app.js
@@ -1,37 +1,72 @@
-// Initial task data stored in JSON format
-let tasks = [
-    { id: 1, text: "Buy groceries" },
-    { id: 2, text: "Complete homework" },
-];
+document.addEventListener("DOMContentLoaded", function () {
+    const taskInput = document.getElementById("task");
+    const addTaskButton = document.getElementById("addTask");
+    const taskList = document.getElementById("taskList");
 
-const taskList = document.getElementById("taskList");
-const addTaskButton = document.getElementById("addTask");
-const taskInput = document.getElementById("task");
+    const taskID = document.getElementById("taskID");
+    const taskText = document.getElementById("taskText");
+    const updateTaskButton = document.getElementById("updateTask");
 
-function renderTasks() {
-    // Clear existing tasks
-    taskList.innerHTML = "";
-
-    // Render tasks from the JSON array
-    tasks.forEach(task => {
-        const li = document.createElement("li");
-        li.textContent = task.text;
-        li.className = "mb-2";
-        taskList.appendChild(li);
-    });
-}
-
-function addTask() {
-    const taskText = taskInput.value.trim();
-    if (taskText !== "") {
-        const newTask = { id: tasks.length + 1, text: taskText };
-        tasks.push(newTask);
-        taskInput.value = "";
-        renderTasks();
+    // Load tasks from the JSON Server
+    function loadTasks() {
+        fetch("http://localhost:3001/tasks")
+            .then(response => response.json())
+            .then(data => {
+                updateTaskList(data);
+            })
+            .catch(error => {
+                console.error("Error loading tasks:", error);
+            });
     }
-}
 
-addTaskButton.addEventListener("click", addTask);
+    
+    function updateTaskList(tasks) {
+        // Clear the task list
+        taskList.innerHTML = "";
 
-// Initial rendering of tasks
-renderTasks();
+        // Add tasks to the list
+        tasks.forEach(task => {
+            const listItem = document.createElement("li");
+            listItem.appendChild(document.createTextNode(task.text));
+            listItem.style.display("flex-direction: row");
+            var button = document.createElement("button");
+            button.innerHTML = "Delete";
+        
+            listItem.appendChild(button);
+            //listItem.textContent = task.text;
+            taskList.appendChild(listItem);
+
+            // button.innerHTML = "asdasd";
+            // li.appendChild(button);
+            // li.setAttribute("id","element4");
+            // ul.appendChild(li);
+            // alert(li.id);
+        });
+    }
+
+    addTaskButton.addEventListener("click", function () {
+        const taskText = taskInput.value;
+        if (taskText) {
+            // Add the new task to the JSON Server
+            fetch("http://localhost:3001/tasks", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ text: taskText }),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    // Load tasks after adding a new task
+                    loadTasks();
+                    taskInput.value = "";
+                })
+                .catch(error => {
+                    console.error("Error adding task:", error);
+                });
+        }
+    });
+
+    // Load tasks when the page loads
+    loadTasks();
+});
